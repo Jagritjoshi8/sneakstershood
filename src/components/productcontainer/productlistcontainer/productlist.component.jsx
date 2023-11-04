f7d7d037a8b387cd15304ec144e7b92f81b5cbe6
Jagritjoshi8 <jagritjoshi268@gmail.com>
2023-11-04 18:02:26 +0530

    filter component integeration

--- src/components/productcontainer/productlistcontainer/productlist.component.jsx
+++ src/components/productcontainer/productlistcontainer/productlist.component.jsx
@@ -1,21 +1,36 @@
 import "./productlist.styles.scss";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { ProductsContext } from "../../../contexts/products.context";
 import ProductCard from "../productcardcontainer/productcard.component";
 import { useGetAllProductsQuery } from "../../../features/productsApi";
+import { getPricedProducts } from "../../../helpers/filter-functions/price";
+import { getRatedProducts } from "../../../helpers/filter-functions/ratings";
+import { getCategoryWiseProducts } from "../../../helpers/filter-functions/category";
+import { getSortedProducts } from "../../../helpers/filter-functions/sort";
 const ProductListContainer = () => {
   // const { products } = useContext(ProductsContext);
+  const { price, rating, categories, sort } = useSelector(
+    (state) => state.productfilters
+  );
   const { data, error, isLoading } = useGetAllProductsQuery();
+
+  const ratedProducts = getRatedProducts(data, rating);
+
+  const categoryProducts = getCategoryWiseProducts(ratedProducts, categories);
+
+  const pricedProducts = getPricedProducts(categoryProducts, price);
+
+  const sortedProducts = getSortedProducts(pricedProducts, sort);
+
   return (
     <div className="product-card-container">
-      {isLoading ? (
-        <p>loading...</p>
-      ) : error ? (
-        <p>erorr..</p>
+      {!sortedProducts ? (
+        <p>no matching products</p>
       ) : (
         <>
-          {data.map((data) => {
+          {sortedProducts.map((data) => {
             return <ProductCard data={data} key={data.id} />;
           })}
         </>
